b1da11056398f09c2a87e5d1ed6d44b6f2e7384e
Burak Ok <burakok@microsoft.com>
2024-10-08 15:30:13 +0200

    pid,sleep

--- z.go
+++ z.go
@@ -2,15 +2,22 @@ package zzz
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func init() {
-	for {
-		time.Sleep(10 * time.Second)
-		a := fmt.Sprintf("Hello")
-		if a == "asd" {
-			break
-		}
-	}
+	fmt.Printf("pid: %d\n", os.Getpid())
+	fmt.Printf("ppid: %d\n", os.Getppid())
+
+	time.Sleep(20 * time.Second)
+
+	fmt.Printf("sleep done\n")
+	// for {
+	// 	time.Sleep(10 * time.Second)
+	// 	a := fmt.Sprintf("Hello")
+	// 	if a == "asd" {
+	// 		break
+	// 	}
+	// }
 }
